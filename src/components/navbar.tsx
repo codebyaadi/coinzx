d357e151c4e21e2116e24af3a3c675229a8c8f84
codebyaadi <adityastar57@gmail.com>
2024-09-24 23:00:21 +0530

    ui: modified the navbar for user button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,22 +4,30 @@ import React from "react";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const Navbar = () => {
   const session = useSession();
 
   return (
     <nav className="w-full font-prompt">
-      <div className="item-center mx-auto flex max-w-full md:max-w-4xl lg:max-w-7xl justify-between py-2 px-4">
-        <div id="logo">
+      <div className="item-center mx-auto flex max-w-full justify-between border-b px-4 py-3 md:max-w-4xl lg:max-w-7xl">
+        <div id="logo" className="flex items-center justify-start">
           <Link href="/">
             <h1 className="font-unbounded text-base font-semibold text-orange-400">CoinzX</h1>
           </Link>
         </div>
         {session.data?.user ? (
-          <Button variant="destructive" className="rounded-full" onClick={() => signOut()}>
-            Log out
-          </Button>
+          <UserButton />
         ) : (
           <div className="flex justify-center gap-2">
             <Button variant="ghost" className="rounded-full" onClick={() => signIn()}>
@@ -39,3 +47,39 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+const UserButton = () => {
+  const session = useSession();
+  const user = session.data?.user;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar>
+          <AvatarImage
+            src={user?.image || "/default-avatar.png"}
+            alt={user?.name || "User Avatar"}
+          />
+          <AvatarFallback>{user?.name?.charAt(0) || "U"}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="mr-8 w-44 font-prompt">
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <span>Profile</span>
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <span>Billing</span>
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>
+          <span>Log out</span>
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
